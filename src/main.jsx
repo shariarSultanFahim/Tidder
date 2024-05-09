51c40dfcbd1851c976e7ba4ede7db5f537c5ef0c
shariarsultanfahim <shariarsfahim@gmail.com>
2024-05-09 19:15:04 +0600

    Basic folder structure and Error Route Page is added

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Error from "./Components/Error/Error";
+import Root from "./Components/Root/Root";
+import Home from "./Components/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root/>,
+    errorElement:<Error/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>
+      }
+    ]
   },
 ]);
 
